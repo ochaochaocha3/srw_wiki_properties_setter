7bae3422bce1fa098a5ebf6f949aa1f2eb2f90c1
ocha <ochaochaocha3@gmail.com>
2016-07-02 23:31:29 +0900

    プロパティセッターを共通化する

--- character.rb
+++ character.rb
@@ -1,38 +1,34 @@
 class CharacterPropertiesSetter
+  def self.simple_property_setter(label)
+    ->(value) { "[[#{label}::#{value}]]" }
+  end
+
+  def self.property_with_unit_setter(label)
+    lambda { |value|
+      value.gsub(/(\d+)\s*(\w+)/) { "[[#{label}::#{$1} #{$2}]]" }
+    }
+  end
+
   TEMPLATE_SETTER = {
     '声優' => lambda { |value|
       "{{声優|#{value}}}"
     },
-    '種族' => lambda { |value|
-      "[[種族::#{value}]]"
-    },
-    '性別' => lambda { |value|
-      "[[性別::#{value}]]"
-    },
+    '種族' => simple_property_setter('種族'),
+    '性別' => simple_property_setter('性別'),
     '年齢' => lambda { |value|
       value.gsub(/(\d+)歳/) { "[[年齢::#{$1}]]歳" }
     },
-    '身長' => lambda { |value|
-      value.gsub(/(\d+)\s*(\w+)/) { "[[身長::#{$1} #{$2}]]" }
-    },
-    '体重' => lambda { |value|
-      value.gsub(/(\d+)\s*(\w+)/) { "[[体重::#{$1} #{$2}]]" }
-    },
+    '身長' => property_with_unit_setter('身長'),
+    '体重' => property_with_unit_setter('体重'),
     '血液型' => lambda { |value|
       value.gsub(/(.+?)型/) { "[[血液型::#{$1}]]型" }
     },
     '所属' => lambda { |value|
       value.gsub(/\[\[(.+?)\]\]/) { "{{所属 (人物)|#{$1}}}" }
     },
-    '階級' => lambda { |value|
-      "[[階級::#{value}]]"
-    },
-    '役職' => lambda { |value|
-      "[[役職::#{value}]]"
-    },
-    '称号' => lambda { |value|
-      "[[称号::#{value}]]"
-    },
+    '階級' => simple_property_setter('階級'),
+    '役職' => simple_property_setter('役職'),
+    '称号' => simple_property_setter('称号'),
     '主な搭乗機' => lambda { |value|
       value.gsub(/\[\[(.+?)\]\]/) { "[[搭乗機::#{$1}]]" }
     }
