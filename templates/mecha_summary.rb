7fa7b53faaeea4d41f3ae2164ec408a88fdc0a9f
ocha <ochaochaocha3@gmail.com>
2017-01-12 23:57:11 +0900

    登場メカ概要テンプレートの生成器を追加する

---
+++ templates/mecha_summary.rb
@@ -0,0 +1,9 @@
+lib_dir = File.expand_path('../lib', File.dirname(__FILE__))
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require 'srw_wiki/summary_template/generator'
+
+generator = SrwWiki::SummaryTemplate::Generator.new(
+  'mecha_summary.yml', 'summary_template.erb'
+)
+puts(generator.generate)
